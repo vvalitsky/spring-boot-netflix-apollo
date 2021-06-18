8e1fcc303c0a585170f0219cdeec2140107a7a92
Vladislav Valitsky <Vladislav.VALITSKY@raiffeisen.ru>
2021-06-18 17:40:13 +0600

    Refactor

--- employee-car-service/src/main/kotlin/com/vvalitsky/config/ScalarsRegistrationConfiguration.kt
+++ employee-car-service/src/main/kotlin/com/vvalitsky/config/ScalarsRegistrationConfiguration.kt
@@ -2,7 +2,6 @@ package com.vvalitsky.config
 
 import com.netflix.graphql.dgs.DgsComponent
 import com.netflix.graphql.dgs.DgsRuntimeWiring
-import graphql.Scalars
 import graphql.scalars.ExtendedScalars
 import graphql.schema.idl.RuntimeWiring
 
@@ -24,18 +23,14 @@ class ScalarsRegistrationConfiguration {
         // The is a non standard scalar and is difficult for clients
         // (such as browser and mobile code) to cope with the exact semantics.
         // These will be removed in the future version and moved to another library.
-        return builder.scalar(Scalars.GraphQLLong)
+        return builder.scalar(ExtendedScalars.GraphQLLong)
     }
+
     /**
      * DateTime scalar add.
      */
     @DgsRuntimeWiring
     fun addLocalDateTimeScalar(builder: RuntimeWiring.Builder): RuntimeWiring.Builder? {
-        // TODO Solve this deprecation
-        // This represents the "Long" type which is a representation of java.lang.Long
-        // The is a non standard scalar and is difficult for clients
-        // (such as browser and mobile code) to cope with the exact semantics.
-        // These will be removed in the future version and moved to another library.
         return builder.scalar(ExtendedScalars.DateTime)
     }
 }
